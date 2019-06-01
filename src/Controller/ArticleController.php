59daa638cb3c7182ada9e1fece9a3103c1b861d9
Jon Atxa <atxajon@gmail.com>
2019-06-01 16:13:11 +0200

    added sec checker and twig

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -1,18 +1,13 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: jonatxa
- * Date: 01/06/2019
- * Time: 12:29
- */
 
 namespace App\Controller;
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ArticleController {
+class ArticleController extends AbstractController {
 
   /**
    * @Route("/")
@@ -34,6 +29,16 @@ class ArticleController {
    * @Route("/blog/{id}")
    */
   public function blogPages($id) {
-    return new Response('Blog page for id: ' . $id);
+    $comments = [
+      'I ate a normal rock once. It did NOT taste like bacon!',
+      'Woohoo! I\'m going on an all-asteroid diet!',
+      'I like bacon too! Buy some from my site! bakinsomebacon.com',
+    ];
+
+    return $this->render('article/show.html.twig', [
+      'title' => 'Blog page',
+      'page_id' => ucwords($id),
+      'comments' => $comments,
+    ]);
   }
 }
